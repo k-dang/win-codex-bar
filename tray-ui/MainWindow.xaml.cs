52cb18e2123120d0eaa8b668a275e587115c2bb8
Kevin Dang <dangkevin12@gmail.com>
2026-01-29 00:54:13 -0500

    feat: stylize

--- tray-ui/MainWindow.xaml.cs
+++ tray-ui/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System;
+using Microsoft.UI.Composition;
+using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI;
 using Microsoft.UI.Dispatching;
 using Microsoft.UI.Windowing;
@@ -7,20 +9,25 @@ using Microsoft.UI.Xaml.Controls;
 using tray_ui.Models;
 using tray_ui.Services;
 using tray_ui.ViewModels;
+using Windows.UI;
+using WinRT;
 using WinRT.Interop;
 
 namespace tray_ui;
 
 public sealed partial class MainWindow : Window
 {
-    private const int DefaultWindowWidth = 900;
+    private const int DefaultWindowWidth = 420;
     private const int MinWindowHeight = 420;
     private const int MaxWindowHeight = 720;
-    private const int WindowChromeHeight = 48;
+    private const int WindowChromeHeight = 8;
     private readonly UsageMonitor _monitor;
     private AppWindow? _appWindow;
+    private AppWindowTitleBar? _titleBar;
     private bool _allowClose;
     private bool _resizePending;
+    private DesktopAcrylicController? _acrylicController;
+    private SystemBackdropConfiguration? _backdropConfiguration;
 
     public MainViewModel ViewModel { get; } = new();
 
@@ -29,14 +36,21 @@ public sealed partial class MainWindow : Window
         _monitor = monitor;
         InitializeComponent();
 
-        ExtendsContentIntoTitleBar = false;
-        SetInitialWindowSize(DefaultWindowWidth, 600);
         InitializeWindowEvents();
+        ConfigureCustomTitleBar();
+        SetInitialWindowSize(DefaultWindowWidth, 600);
         RootGrid.Loaded += MainWindow_Loaded;
+        RootGrid.ActualThemeChanged += RootGrid_ActualThemeChanged;
+        Activated += MainWindow_Activated;
+        Closed += MainWindow_Closed;
+        SizeChanged += MainWindow_SizeChanged;
 
         RootGrid.DataContext = ViewModel;
         ViewModel.Update(_monitor.Summary);
         _monitor.SummaryUpdated += OnSummaryUpdated;
+
+        TrySetThinAcrylicBackdrop();
+        UpdateTitleBarStyle(RootGrid.ActualTheme);
     }
 
     public void ShowWindow()
@@ -189,6 +203,64 @@ public sealed partial class MainWindow : Window
         }
     }
 
+    private void ConfigureCustomTitleBar()
+    {
+        if (_appWindow == null || !AppWindowTitleBar.IsCustomizationSupported())
+        {
+            ExtendsContentIntoTitleBar = false;
+            return;
+        }
+
+        ExtendsContentIntoTitleBar = true;
+        _titleBar = _appWindow.TitleBar;
+        _titleBar.ExtendsContentIntoTitleBar = true;
+        SetTitleBar(TitleBarDragRegion);
+        UpdateTitleBarInsets();
+    }
+
+    private void UpdateTitleBarInsets()
+    {
+        if (_titleBar == null)
+        {
+            return;
+        }
+
+        TitleBarRightInset.Width = _titleBar.RightInset;
+    }
+
+    private void UpdateTitleBarStyle(ElementTheme theme)
+    {
+        if (_appWindow == null)
+        {
+            return;
+        }
+
+        if (!AppWindowTitleBar.IsCustomizationSupported())
+        {
+            return;
+        }
+
+        var titleBar = _appWindow.TitleBar;
+        var isDark = theme == ElementTheme.Dark || theme == ElementTheme.Default;
+        var foreground = isDark ? Colors.White : Colors.Black;
+        var inactiveForeground = isDark ? Color.FromArgb(180, 255, 255, 255) : Color.FromArgb(160, 0, 0, 0);
+        var hoverBackground = isDark ? Color.FromArgb(36, 255, 255, 255) : Color.FromArgb(24, 0, 0, 0);
+        var pressedBackground = isDark ? Color.FromArgb(56, 255, 255, 255) : Color.FromArgb(40, 0, 0, 0);
+
+        titleBar.BackgroundColor = Colors.Transparent;
+        titleBar.InactiveBackgroundColor = Colors.Transparent;
+        titleBar.ForegroundColor = foreground;
+        titleBar.InactiveForegroundColor = inactiveForeground;
+        titleBar.ButtonBackgroundColor = Colors.Transparent;
+        titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        titleBar.ButtonForegroundColor = foreground;
+        titleBar.ButtonInactiveForegroundColor = inactiveForeground;
+        titleBar.ButtonHoverBackgroundColor = hoverBackground;
+        titleBar.ButtonPressedBackgroundColor = pressedBackground;
+        titleBar.ButtonHoverForegroundColor = foreground;
+        titleBar.ButtonPressedForegroundColor = foreground;
+    }
+
     private void AppWindow_Closing(AppWindow sender, AppWindowClosingEventArgs args)
     {
         if (_allowClose)
@@ -220,6 +292,75 @@ public sealed partial class MainWindow : Window
     private void MainWindow_Loaded(object sender, RoutedEventArgs e)
     {
         ScheduleResizeToContentHeight();
+    }
+
+    private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        if (_backdropConfiguration != null)
+        {
+            _backdropConfiguration.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+        }
+    }
+
+    private void MainWindow_SizeChanged(object sender, WindowSizeChangedEventArgs args)
+    {
+        UpdateTitleBarInsets();
+    }
+
+    private void MainWindow_Closed(object sender, WindowEventArgs args)
+    {
+        if (_acrylicController != null)
+        {
+            _acrylicController.Dispose();
+            _acrylicController = null;
+        }
+
+        _titleBar = null;
+
+        _backdropConfiguration = null;
+    }
+
+    private void RootGrid_ActualThemeChanged(FrameworkElement sender, object args)
+    {
+        if (_backdropConfiguration != null)
+        {
+            _backdropConfiguration.Theme = MapTheme(sender.ActualTheme);
+        }
+
+        UpdateTitleBarStyle(sender.ActualTheme);
+    }
+
+    private bool TrySetThinAcrylicBackdrop()
+    {
+        if (!DesktopAcrylicController.IsSupported())
+        {
+            return false;
+        }
+
+        _backdropConfiguration = new SystemBackdropConfiguration
+        {
+            IsInputActive = true,
+            Theme = MapTheme(RootGrid.ActualTheme)
+        };
+
+        _acrylicController = new DesktopAcrylicController
+        {
+            Kind = DesktopAcrylicKind.Thin
+        };
+
+        _acrylicController.AddSystemBackdropTarget(this.As<ICompositionSupportsSystemBackdrop>());
+        _acrylicController.SetSystemBackdropConfiguration(_backdropConfiguration);
+        return true;
+    }
+
+    private static SystemBackdropTheme MapTheme(ElementTheme theme)
+    {
+        return theme switch
+        {
+            ElementTheme.Dark => SystemBackdropTheme.Dark,
+            ElementTheme.Light => SystemBackdropTheme.Light,
+            _ => SystemBackdropTheme.Default
+        };
     }
 
     private void ScheduleResizeToContentHeight()
